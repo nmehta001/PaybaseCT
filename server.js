11107d69321fa2f66dc23a6ad56ff055bbf9a288
= <=>
2019-02-01 15:17:18 +0000

    Cleanup index.js, remove duplicate code

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const Mongoose = require('mongoose');
 
 const app = new Koa();
 const router = new KoaRouter();
+const assert = require('assert');
 
 app.use(KoaLogger());
 
@@ -15,6 +16,9 @@ require('./code/routes')(router);
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-Mongoose.connect(`mongodb://<dbuser>:<dbpassword>@ds213645.mlab.com:13645/paybase-ct`);
+Mongoose.connect(`mongodb://testuser:paybasect1@ds213645.mlab.com:13645/paybase-ct`, (err, db) => {
+  assert.equal(null, err);
+  console.log("Successfully connected to the database");
+});
 
 module.exports = app;
